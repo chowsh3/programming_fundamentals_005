8172c51d52b38a69e64100549f13e38a26390ee8
chowsh3 <shobir.c@icloud.com>
2020-04-15 02:17:06 +0100

    completed

--- test/sugar.test.js
+++ test/sugar.test.js
@@ -234,7 +234,7 @@ describe("Object.invert", () => {
   });
 });
 
-describe.only("Object.addAll", () => {
+describe("Object.addAll", () => {
   test("Creates a new object out of all the objects in an array", () => {
     const objects = [{ a: 1 }, { b: 3 }, { c: "foo" }];
 
